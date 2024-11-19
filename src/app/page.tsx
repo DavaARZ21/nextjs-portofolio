9c86e1042528066df8f768d69cdf36510dbb8a3e
Dava <davaarz21@gmail.com>
2024-11-19 10:36:53 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Portofolio from "@/components/Portofolio";
+import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Portofolio/>
+      <Skills/>
+      <Contact/>
+      <Footer/>
+    </>
+  );
+}
